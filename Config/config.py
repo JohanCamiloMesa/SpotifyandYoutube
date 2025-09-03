35d2daeeb831a1b939b570b3173949dbdc314a7a
JohanCamiloMesa <jcmesar@academia.usbbog.edu.co>
2025-09-03 11:14:53 -0500

    Features: Create database

--- Config/config.py
+++ Config/config.py
@@ -4,3 +4,5 @@ class Config:
     """
     INPUT_PATH = 'D:\\SpotifyandYoutube\\Extract\\Files\\Spotify_Youtube.csv'
     OUTPUT_PATH = 'D:\\SpotifyandYoutube\\Extract\\Files\\output_clean.csv'
+    SQLITE_DB_PATH = 'Extract/Files/etl_data.db'
+    SQLITE_TABLE = 'ride_bookings_clean'
